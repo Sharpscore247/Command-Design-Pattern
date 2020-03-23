31feb06e263d496d43d8f17874c6eff994656901
Tyron Schultz <tyronsonfire@gmail.com>
2020-03-23 19:02:35 -0400

    added driver

--- Command-Design-Pattern/src/inputHandler.java
+++ Command-Design-Pattern/src/inputHandler.java
@@ -1,4 +1,11 @@
-
+/**
+ * @author Tyron Schultz
+ *
+ * Class to map different key words to commands
+ *
+ */
+import java.util.*;
 public class inputHandler {
-
+	
+	HashMap<String, Command> map = new HashMap();
 }
