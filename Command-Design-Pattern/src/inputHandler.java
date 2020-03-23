b3936973d7ff91c96e3f8bc6b76bbc3aec2bdf5e
Tyron Schultz <tyronsonfire@gmail.com>
2020-03-23 19:10:20 -0400

    update

--- Command-Design-Pattern/src/inputHandler.java
+++ Command-Design-Pattern/src/inputHandler.java
@@ -1,11 +1,14 @@
 /**
  * @author Tyron Schultz
  *
- * Class to map different key words to commands
+ *  Class to map different key words to commands
  *
  */
 import java.util.*;
+
 public class inputHandler {
 	
-	HashMap<String, Command> map = new HashMap();
+	private HashMap<String, Command> map = new HashMap();
+	
+	public 
 }
